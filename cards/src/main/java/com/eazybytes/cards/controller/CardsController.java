93fc5fbabb2df206c78fd55dcf3648f0663f38de
cha <yeonhee1021@gmail.com>
2025-02-24 13:44:16 +0900

    a

--- cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -193,7 +193,7 @@ public ResponseEntity<LoansDto> fetchTotalLoan(@RequestParam
 
 }
 
-@GetMapping("/pod-cards")
+@GetMapping("/podcard")
 public String getMethodName() {
     String podName = System.getenv("HOSTNAME");
     return "Pod Name: " + (podName != null ? podName : "unknown");
